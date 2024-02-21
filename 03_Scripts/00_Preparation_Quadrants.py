5f714593dd670affaf8a471d01f2f91c14b8e1a2
Mathieu <mathieu.simon2012@gmail.com>
2024-02-21 15:01:58 +0100

    Update quadrant lines definition

--- 03_Scripts/00_Preparation_Quadrants.py
+++ 03_Scripts/00_Preparation_Quadrants.py
@@ -23,6 +23,7 @@ import os
 import argparse
 import numpy as np
 import pandas as pd
+from pathlib import Path
 import SimpleITK as sitk
 import matplotlib.pyplot as plt
 from Utils import Time, SetDirectories
@@ -33,7 +34,30 @@ from pylatex.package import Package
 #%% Functions
 # Define functions
 
-def FillHoles(Image, Radius=5):
+def SetDirectories(Name:str):
+
+    """
+    Return full path of the main project folders into a dictionary
+    """
+
+    CWD = str(Path.cwd())
+    Start = CWD.find(Name)
+    WD = Path(CWD[:Start], Name)
+    Dirs = [D for D in WD.iterdir() if D.is_dir()]
+
+    Directories = {}
+    Directories['CWD'] = WD
+    Directories['Data'] = [D for D in Dirs if 'Data' in D.name][0]
+    Directories['Scripts'] = [D for D in Dirs if 'Scripts' in D.name][0]
+    Directories['Results'] = [D for D in Dirs if 'Results' in D.name][0]
+
+    return Directories
+
+def FillHoles(Image:sitk.Image, Radius=5):
+
+    """
+    Fill holes of a binary Simple ITK image
+    """
 
     Dimension = Image.GetDimension()
 
@@ -81,11 +105,19 @@ def FillHoles(Image, Radius=5):
 
     return BinImage
 
-def PrintImage(Image, FileName, pCOG, X_Line, Y_Line, Show=True):
+def PrintImage(Image:sitk.Image, FileName:str, pCOG:tuple,
+               X_Line:np.array, Y_Line:np.array, Show=True):
+
+    """
+    Print picture of uCT scan with lines defining anatomical quadrants
+    """
+    
+    # Get image properties
     ImageSize = np.array(Image.GetSize())
     ImageSpacing = np.array(Image.GetSpacing())
     PhysicalSize = ImageSize * ImageSpacing
 
+    # Set figure properties
     Inch = 25.4
     mm = 1 / Inch
     Subsampling = 1
@@ -98,6 +130,7 @@ def PrintImage(Image, FileName, pCOG, X_Line, Y_Line, Show=True):
                     1 - Margins[0] / 2 * PhysicalSize[0] / (FigureSize[0] * Inch),
                     1 - Margins[1] / 2 * PhysicalSize[1] / (FigureSize[1] * Inch)]
 
+    # Generate figure and save it
     Figure, Axis = plt.subplots(1,1, figsize=FigureSize, dpi=int(DPI))
     Axis.imshow(sitk.GetArrayFromImage(Image), cmap='bone')
     Axis.plot(pCOG[0] + X_Line, pCOG[1] + Y_Line, color=(1, 0, 0))
@@ -114,132 +147,136 @@ def PrintImage(Image, FileName, pCOG, X_Line, Y_Line, Show=True):
     else:
         plt.close(Figure)
 
+
 #%% Main
 # Main part
 
-def Main():
+def Main(Angle=60):
 
-    # Set directory and load sample list
-    WD, DD, SD, RD = SetDirectories('FEXHIP-Histology')
-    SampleList = pd.read_csv(str(DD / 'SampleList.csv'))
-    uCTD = DD / '01_uCTScans'
-    OutD = RD / '01_Quadrants'
-    Angle = 60
-
-    Exceptions = ['389L','397L','427L']
-
+    # Set directories and load donors list
+    Dirs = SetDirectories('FEXHIP-Histology')
+    DataList = pd.read_excel(Dirs['Data'] / 'DonorsList.xlsx', index_col=[0])
+    OutD = Dirs['Results'] / '01_Quadrants'
+    
+    # Iterate over each donor
     Time.Process(1, 'Print lines')
-    for Index, Row in SampleList.iterrows():
+    Indices = pd.MultiIndex.from_product([DataList.index, ['Left','Right']])
+    Widths = pd.DataFrame(index=Indices,columns=['Width (mm)'])
+    for Idx, Row in DataList.iterrows():
 
-        Sample = str(Row['Sample ID'])
-        uCT = Row['uCT'] + '.mhd'
-        Text = 'Sample ' + Sample + ' ' + Row['Side']
-        Time.Update(Index / len(SampleList), Text)
+        Time.Update(Idx / len(DataList), 'Donor ' + f"{Idx:02d}")
 
-        # Read corresponding sample uct
-        Scan = sitk.ReadImage(str(uCTD / uCT))
-        Array = sitk.GetArrayFromImage(Scan)
+        # List uCT scans
+        Donor = 'Donor_' + f"{Idx:02d}"
+        DonorDir = Dirs['Data'] / Donor
+        Scans = [F for F in DonorDir.iterdir() if F.name.endswith('.mhd')]
 
-        # Binarize image to find area centroid
-        OtsuFilter = sitk.OtsuMultipleThresholdsImageFilter()
-        OtsuFilter.SetNumberOfThresholds(2)
-        BinImage = OtsuFilter.Execute(Scan)
+        # Iterate over each scan
+        for File in Scans:
 
-        # Perform binary dilation and erosion to find center for proximal and distal slices
-        Filled = FillHoles(BinImage)
+            # Read corresponding sample uct
+            Scan = sitk.ReadImage(str(File))
+            Array = sitk.GetArrayFromImage(Scan)
 
-        # Use distal slice for orientation computation
-        Slice = int(round(Filled.GetSize()[2]*0.1))
+            # Binarize image to find area centroid
+            OtsuFilter = sitk.OtsuMultipleThresholdsImageFilter()
+            OtsuFilter.SetNumberOfThresholds(2)
+            BinImage = OtsuFilter.Execute(Scan)
 
-        if Sample + Row['Side'] in Exceptions:
-            Slice = -Slice
+            # Perform binary dilation and erosion to find center for proximal and distal slices
+            Filled = FillHoles(BinImage)
 
-        # Get orientation of filled slice
-        LabStat = sitk.LabelIntensityStatisticsImageFilter()
-        LabStat.Execute(Filled[:,:,Slice], Filled[:,:,Slice])
-        COGFilled = LabStat.GetCenterOfGravity(2)
-        X1, Y1, X2, Y2 = LabStat.GetPrincipalAxes(2)
-        OrientationAngle = -np.arctan(-X1 / Y1)
+            # Use distal slice for orientation computation
+            Slice = int(round(Filled.GetSize()[2]*0.1))
 
-        # Create according rotation transform
-        R = np.array([[np.cos(OrientationAngle), -np.sin(OrientationAngle), 0],
-                      [np.sin(OrientationAngle),  np.cos(OrientationAngle), 0],
-                      [                       0,                         0, 1]])
+            # Get orientation of filled slice
+            LabStat = sitk.LabelIntensityStatisticsImageFilter()
+            LabStat.Execute(Filled[:,:,Slice], Filled[:,:,Slice])
+            COGFilled = LabStat.GetCenterOfGravity(2)
+            X1, Y1, X2, Y2 = LabStat.GetPrincipalAxes(2)
+            OrientationAngle = -np.arctan(-X1 / Y1)
 
-        Transform = sitk.AffineTransform(3)
-        Transform.SetMatrix(R.ravel())
-        Transform.SetCenter((COGFilled[0], COGFilled[1], 0))
-        Transform.SetTranslation((0, 0, 0))
+            # Create according rotation transform
+            R = np.array([[np.cos(OrientationAngle), -np.sin(OrientationAngle), 0],
+                        [np.sin(OrientationAngle),  np.cos(OrientationAngle), 0],
+                        [                       0,                         0, 1]])
 
-        # Resample image (and pad to avoid out-of-view bone parts)
-        Resampler = sitk.ResampleImageFilter()
-        Pad = 50
-        Resampler.SetReferenceImage(sitk.ConstantPad(Scan,(Pad,Pad,0),(Pad,Pad,0)))
-        Resampler.SetTransform(Transform.GetInverse())
-        Rotated = Resampler.Execute(Scan)
+            Transform = sitk.AffineTransform(3)
+            Transform.SetMatrix(R.ravel())
+            Transform.SetCenter((COGFilled[0], COGFilled[1], 0))
+            Transform.SetTranslation((0, 0, 0))
 
-        # Check if inferior if on left
-        BinImage = OtsuFilter.Execute(Rotated)
-        LabStat.Execute(BinImage[:,:,Slice], BinImage[:,:,Slice])
-        COGBin = LabStat.GetCenterOfGravity(2)
-
-        if COGBin[0] > COGFilled[0]:
-            Transform.SetMatrix([-1, 0, 0, 0, -1, 0, 0, 0, 1])
+            # Resample image (and pad to avoid out-of-view bone parts)
+            Resampler = sitk.ResampleImageFilter()
+            Pad = 50
+            Resampler.SetReferenceImage(sitk.ConstantPad(Scan,(Pad,Pad,0),(Pad,Pad,0)))
             Resampler.SetTransform(Transform.GetInverse())
-            Rotated = Resampler.Execute(Rotated)
+            Rotated = Resampler.Execute(Scan)
 
-        # Filter and crop imag to bone
-        Gauss = sitk.DiscreteGaussianImageFilter()
-        Gauss.SetVariance(0.05)
-        Gauss.SetMaximumKernelWidth(10)
-        Smooth = Gauss.Execute(Rotated[:,:,Slice])
+            # Check if inferior if on left
+            BinImage = OtsuFilter.Execute(Rotated)
+            LabStat.Execute(BinImage[:,:,Slice], BinImage[:,:,Slice])
+            COGBin = LabStat.GetCenterOfGravity(2)
 
-        BinImage = OtsuFilter.Execute(Smooth)
-        Array = sitk.GetArrayFromImage(BinImage)
-        Coords = np.where(Array == 2)
-        Cropped = BinImage[Coords[1].min()-5: Coords[1].max()+5,
-                         Coords[0].min()-5: Coords[0].max()+5]
+            if COGBin[0] > COGFilled[0]:
+                Transform.SetMatrix([-1, 0, 0, 0, -1, 0, 0, 0, 1])
+                Resampler.SetTransform(Transform.GetInverse())
+                Rotated = Resampler.Execute(Rotated)
 
-        # Get center of gravity (COG) of filled slice
-        COG = Filled[:,:,Slice].TransformPhysicalPointToIndex(COGFilled)
-        COG = (COG[0] - Coords[1].min() + Pad,
-               COG[1] - Coords[0].min() + Pad)
+            # Filter and crop imag to bone
+            Gauss = sitk.DiscreteGaussianImageFilter()
+            Gauss.SetVariance(0.05)
+            Gauss.SetMaximumKernelWidth(10)
+            Smooth = Gauss.Execute(Rotated[:,:,Slice])
+
+            BinImage = OtsuFilter.Execute(Smooth)
+            Array = sitk.GetArrayFromImage(BinImage)
+            Coords = np.where(Array == 2)
+            Cropped = BinImage[Coords[1].min()-5: Coords[1].max()+5,
+                            Coords[0].min()-5: Coords[0].max()+5]
+
+            # Get center of gravity (COG) of filled slice
+            COG = Filled[:,:,Slice].TransformPhysicalPointToIndex(COGFilled)
+            COG = (COG[0] - Coords[1].min() + Pad,
+                COG[1] - Coords[0].min() + Pad)
 
 
-        # Compute cutting lines
-        X_Line = np.linspace(0,Cropped.GetSize()[0],100)
-        Y_Line = X_Line * np.tan(Angle/2 * np.pi/180)
+            # Compute cutting lines
+            X_Line = np.linspace(0,Cropped.GetSize()[0],100)
+            Y_Line = X_Line * np.tan(Angle/2 * np.pi/180)
 
-        # Print image with correct size
-        FileName = str(OutD / (Sample + '_' + Row['Side'] + '.png'))
-        PrintImage(1 - (Cropped==2), FileName, COG, X_Line, Y_Line, Show=False)
+            # Print image with correct size
+            FileName = str(OutD / (Donor + File.name[4:-4] + '.png'))
+            PrintImage(1 - (Cropped==2), FileName, COG, X_Line, Y_Line, Show=False)
 
-        SampleList.loc[Index, 'Width'] = Cropped.GetSize()[0] * Cropped.GetSpacing()[0]
+            # Store physical width
+            Width = Cropped.GetSize()[0] * Cropped.GetSpacing()[0]
+            Widths.loc[(Idx, File.name[5:-4]), 'Width (mm)'] = Width
 
     Time.Process(0, 'Lines printed!')
 
     # Write pdf file
     Time.Process(1, 'Write PDF')
-    Files = [F for F in os.listdir(OutD) if F.endswith('.png')]
-    Files.sort()
+    Files = sorted([F for F in OutD.iterdir()])
     Factor = 1.2
 
-    Doc = Document(default_filepath=str(RD / '01_Quadrants'))
+    Doc = Document(str(OutD))
     for Index, File in enumerate(Files):
 
-        Time.Update(Index / len(Files))
-
-        Image = str(OutD / File)
+        Time.Update((Index+1) / len(Files))
+        _, Donor, Side = File.name[:-4].split('_')
+        Loc = (int(Donor),Side)
+        SubCap = 'Donor ' + str(int(Donor)) + ' ' + Side
 
         if np.mod(Index, 2) == 0:
-            Images = [Image]
-            SubCaptions = [File[:-4]]
-            Width = [str(round(SampleList.loc[Index,'Width'] * Factor,1))]
+            Images = [File]
+            SubCaptions = [SubCap]
+            Width = [str(round(Widths.loc[Loc,'Width (mm)'] * Factor,1))]
         
         else:
-            Images.append(Image)
-            SubCaptions.append(File[:-4])
-            Width.append(str(round(SampleList.loc[Index,'Width'] * Factor,1)))
+            Images.append(File)
+            SubCaptions.append(SubCap)
+            Width.append(str(round(Widths.loc[Loc,'Width (mm)'] * Factor,1)))
 
 
             with Doc.create(Figure(position='h!')):
@@ -247,16 +284,14 @@ def Main():
                 for i, Image in enumerate(Images):
                     SubFig = SubFigure(position='b',width=NoEscape(Width[i] + ' mm'))
                     with Doc.create(SubFig) as SF:
-                        SF.add_image(Image)
+                        SF.add_image(str(Image))
                         SF.add_caption(SubCaptions[i])
                     
                     if i == 0:
                         Doc.append(Command('hfill'))
 
     Doc.packages.append(Package('subcaption', options='aboveskip=0pt, labelformat=empty'))
-            
-    Doc.generate_pdf(clean_tex=False)
-
+    Doc.generate_pdf(clean_tex=True)
     Time.Process(0)
 
 #%% If main
